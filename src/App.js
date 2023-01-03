e80fe9057947866cffa4c6bde6cbf612f873aadd
AK <AK18@ak.com>
2023-01-03 20:01:32 +0530

    Routing changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import { } from "antd";
 import './App.css';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useNavigate } from "react-router-dom";
 import AppLayout from "./layout";
-import MoviesList from "./Components/MoviesList";
+import { useEffect } from "react";
 
 function App() {
+  const navigate = useNavigate();
+  useEffect(() => {
+
+    const navigateToHome = () => {
+      navigate("/");
+    };
+    navigateToHome();
+  }, [])
   return (
     <div className="App">
       <BrowserRouter>
         <AppLayout />
-        <MoviesList />
       </BrowserRouter>
     </div>
   );
