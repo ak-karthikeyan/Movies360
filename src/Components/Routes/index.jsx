9c28df7b282cea09b44c67a7bdc52690e52ce9b5
AK <AK18@ak.com>
2023-01-03 20:14:34 +0530

    Routing changes

--- src/Components/Routes/index.jsx
+++ src/Components/Routes/index.jsx
@@ -7,7 +7,7 @@ function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<MoviesList />}></Route>
-      <Route path="/Movies360" element={<MoviesList />}></Route>
+      <Route path="/home" element={<MoviesList />}></Route>
       <Route path="/AdminLogin" element={<AdminLogin />}></Route>
       <Route path="/AdminPage" element={<AdminPage />}></Route>
       <Route
